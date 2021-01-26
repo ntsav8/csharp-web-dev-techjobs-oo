f5e1471b761f3433abbf99b67139856eb73b9147
Nita Savage <nitasavage@gmail.com>
2021-01-26 14:39:06 -0600

    Hopefully final change

--- TechJobsOO/Program.cs
+++ TechJobsOO/Program.cs
@@ -24,7 +24,7 @@ namespace TechJobsOO
             {
 
                 Console.WriteLine(job);
-                Console.WriteLine("                     ");
+                
                 
             }
 
